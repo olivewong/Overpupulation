bd9ef93ebbe39096734b941b9b3f72da16901ca8
Daniel Do <danieldo@Daniels-MacBook-Pro-2.local>
2017-02-18 22:28:42 -0800

    Added animations

--- scripts/main.js
+++ scripts/main.js
@@ -6,17 +6,19 @@ function Dog(name, intro, decisions, adoptionChance, critic) {
     this.critic = critic;
 }
 
-var lose = "Despite her good behavior and loving demeanor, your dog was not adopted by any of her visitors - and she’s not the only one. Each year, animal shelters in the U.S. take in 7.6 million pets, and only about a third of them get adopted out. The remainder are either put down or left in overcrowded shelters and receive insufficient care and attention due to lack of human resources. Many people looking to become dog owners look to pet shops for younger, supposedly better-behaved dogs. These pet stores, however, often purchase their pets from puppy mills, where dogs are raised in cramped, filthy conditions and shipped across the country without adequate food, water, ventilation, or shelter - far from the ideal conditions for raising a well-behaved, happy pupper. ";
-var win = "Congratulations! Your dog has been adopted. Unfortunately, they are one of the lucky ones. Each year, animal shelters in the U.S. take in 7.6 million pets, and only about a third of them get adopted out. The remainder are either put down or left in overcrowded shelters and receive insufficient care and attention due to lack of human resources. Many people looking to become dog owners look to pet shops for younger, supposedly better-behaved dogs. These pet stores, however, often purchase their pets from puppy mills, where dogs are raised in cramped, filthy conditions and shipped across the country without adequate food, water, ventilation, or shelter - far from the ideal conditions for raising a well-behaved, happy pupper.";
-var moreInfo = "test";
+var lose = "Despite her good behavior and loving demeanor, Cooper was not adopted by any of her visitors, and she’s not the only one. Each year, animal shelters in the U.S. take in 7.6 million pets, and only about a third of them get adopted. The remainder are either put down or left in overcrowded shelters, and receive insufficient care and attention due to lack of human resources. Many people looking to become dog owners look to pet shops for younger, supposedly better-behaved dogs. These pet stores, however, often purchase their pets from puppy mills, where dogs are raised in cramped, filthy conditions and shipped across the country without adequate food, water, ventilation, or shelter - far from the ideal conditions for raising a well-behaved, happy puppy. Furthermore, every sheltered dog does not fulfill the stereotype of the stubborn ill-behaved animal. According to a study in 2016 (Wallis, Veranyi, etc.), older dogs can still learn, albeit at a slower pace than younger dogs. Many adopters can also testify to the loyalty and love that sheltered pets have offered them throughout the years.";
+var win = "Unfortunately, Cooper is one of the lucky ones. Each year, animal shelters in the U.S. take in 7.6 million pets, and only about a third of them get adopted. The remainder are either put down, or left in overcrowded shelters and receive insufficient care and attention due to lack of human resources. Many people looking to become dog owners look to pet shops for younger, supposedly better-behaved dogs. These pet stores, however, often purchase their pets from puppy mills, where dogs are raised in cramped, filthy conditions and shipped across the country without adequate food, water, ventilation, or shelter - far from the ideal conditions for raising a well-behaved, happy puppy. Furthermore, every sheltered dog does not fulfill the stereotype of the stubborn ill-behaved animal. According to a study in 2016 (Wallis, Veranyi, etc.), older dogs can still learn, albeit at a slower pace than younger dogs. Many adopters can also testify to the loyalty and love that sheltered pets have offered them throughout the years.";
+var moreInfo = "Adopt sheltered pets instead of supporting puppy mills. Donate. Foster";
 var positiveResponses = ["Awww",  "So cute!", "Wow!", "That's great!", "Good dog!", "Amazing.", "Adorable!"];
 var negativeResponses = ["Bad dog!", "Ow!", "I knew sheltered dogs would be like this!", "I wouldn't want to be this dog's friend.", "It'd be hard to take care of this dog."];
 var criticNames = ["Olivia", "Vanessa", "Heidi", "Daniel"];
+var criticIcons = ["images/olivia.png", "images/vanessa.png", "images/heidi.png", "images/daniel.png"];
 var instructions = [" is visiting the shelter today. Try to impress! Choose an action to the left."]; 
 var dogChances = [0.0, .6, 1.0, .3];
 var totalCritics = 4;
 var criticIndex = 0;
 var actionOk = true;
+var result = false;
 var dogs = [];
 var myDog;
 /*
@@ -29,7 +31,7 @@ var myDog;
 */
 
 function initializeDogs() {
-	dogs[0]  = new Dog("cooper", "Meet Cooper! Cooper is a four year-old female pitbull. She was purchased from a pet store as a puppy but was left behind in the city when her former owners moved. She’s been a resident of the shelter for the past six months and is looking for a new home. Cooper loves chew toys, playing fetch, and taking naps in the sun. Try to get Cooper adopted!", ["Unfortunately, Olivia decides not to adopt Cooper. Although Cooper is energetic, Olivia wants a younger dog, so she will go probably to go a pet store. This leaves many perfectly trainable and loveable dogs like Cooper stuck in shelters.", "Sadly, Vanessa decides not to adopt Cooper. She doesn’t like that Cooper is a pitbull, and wants to look for a different breed, probably purebred. Like many potential adopters, Vanessa has misconceptions about pitbulls in general, even though many of them don’t have behavioral problems.", "Unfortunately, Heidi also decides not to adopt Cooper. She is scared that Cooper will have behavioral problems after being abandoned by her previous owners, and doesn’t want to deal with re-training an adopted dog. In reality, her belief in the saying “old dogs can’t learn new tricks” is totally unfounded. Dogs never stop learning, and even if Heidi were to purchase a puppy, she would still have to train it."], dogChances[0], criticIndex);
+	dogs[0]  = new Dog("cooper", "Meet Cooper! Cooper is a four year-old female pitbull. She was purchased from a pet store as a puppy, but was left behind in the city when her owners moved. She has been a shelter resident for the past six months and is looking for a new home. Cooper loves chew toys, playing fetch, and taking naps in the sun. She’s a loveable and loyal dog that also wants to be loved by a new family. Try to get adopted!", ["Unfortunately, Olivia decides not to adopt Cooper. Although Cooper is energetic, Olivia wants a younger dog, so she will go probably to go a pet store. This leaves many perfectly trainable and loveable dogs like Cooper stuck in shelters.", "Sadly, Vanessa decides not to adopt Cooper. She doesn’t like that Cooper is a pitbull, and wants to look for a different breed, probably purebred. Like many potential adopters, Vanessa has misconceptions about pitbulls in general, even though many of them don’t have behavioral problems.", "Unfortunately, Heidi decides not to adopt Cooper. She is scared that Cooper will have behavioral problems after being abandoned by her previous owners, and doesn’t want to deal with re-training an adopted dog. In reality, her belief in the saying “old dogs can’t learn new tricks” is totally unfounded. Dogs never stop learning, and even if Logan were to purchase a puppy, she would still have to train that puppy.", "Daniel decides not to adopt Cooper because he doesn’t know Cooper’s history, and doesn’t want to take a chance on a sheltered animal. Most of the time, however, pets are left behind because their owners can’t afford to take care of them anymore, not necessarily because of behavioral issues. All Cooper really needs is some love and care, and a new home!", "Daniel decides to adopt Cooper. Cooper seems like he loves being with people, and would be a loyal companion. Cooper also looks like a dog that would be compatible with his dogs at home. Congratulations!"], dogChances[0], criticIndex);
 	dogs[1] = new Dog("luna",  "intro", ["Olivia", "Vanessa", "Heidi", "Daniel"], dogChances[1], criticIndex);
 	dogs[2] = new Dog("loki",  "intro", ["Olivia", "Vanessa", "Heidi", "Daniel"], dogChances[2], criticIndex);
 	dogs[3] = new Dog("ace",  "intro", ["Olivia", "Vanessa", "Heidi", "Daniel"], dogChances[3], criticIndex);
@@ -80,6 +82,28 @@ function initializeDogIntro() {
 	});
 }
 
+function generatePositiveComment() {
+    if (myDog.critic + 1 == totalCritics && result) {
+        $('#comments p').html("<q>" + positiveResponses[parseInt((Math.random() * positiveResponses.length), 10)] + "</q>"
+			 + "<br>" + myDog.decisions[myDog.critic + 1]);
+    } else {
+        $('#comments p').html("<q>" + positiveResponses[parseInt((Math.random() * positiveResponses.length), 10)] + "</q>"
+			 + "<br>" + myDog.decisions[myDog.critic]);
+    }
+    updateDogOptions();
+}
+
+function generateNegativeComment() {
+    if (myDog.critic + 1 == totalCritics && result) {
+        $('#comments p').html("<q>" + negativeResponses[parseInt((Math.random() * negativeResponses.length), 10)] + "</q>"
+			 + "<br>" + myDog.decisions[myDog.critic + 1]);
+    } else {
+        $('#comments p').html("<q>" + negativeResponses[parseInt((Math.random() * negativeResponses.length), 10)] + "</q>"
+			 + "<br>" + myDog.decisions[myDog.critic]);
+    }
+    updateDogOptions();
+}
+
 function initializeComplex() {
 
 	$('#dog-pic').hide(); //Don't show icon.
@@ -96,36 +120,47 @@ function initializeComplex() {
 	* 3 is Tony
 	*/
     
+    var timeOutID = window.setTimeout(function() {
+            $('#pupper img').attr('src', "images/still-pup.gif");
+        },1000);
     $('#instructions p').html("<br>" + criticNames[myDog.critic] + instructions);
 	$('#picture').text(myDog.critic);
 	$('#fetch').click(function() {
-		$('#comments p').html("<q>" + positiveResponses[parseInt((Math.random() * positiveResponses.length), 10)] + "</q>"
-							 + "<br>" + myDog.decisions[myDog.critic]);
-        updateDogOptions();
+        generatePositiveComment();
+        clearTimeout(timeOutID);
+        $('#pupper img').attr('src', "images/pup-fetch.gif");
 	});
 	$('#roll-over').click(function() {
-		$('#comments p').html("<q>" + positiveResponses[parseInt((Math.random() * positiveResponses.length), 10)] + "</q>"
-			+ "<br>" + myDog.decisions[myDog.critic]);
-         updateDogOptions();
+		generatePositiveComment();
+        timeOutID = window.setTimeout(function() {
+            $('#pupper img').attr('src', "images/still-pup.gif");
+        },1000);
+        $('#pupper img').attr('src', "images/roll-pup.gif");
+        
 	});
 	$('#wag').click(function() {
-		$('#comments p').html("<q>" + positiveResponses[parseInt((Math.random() * positiveResponses.length), 10)] + "</q>"
-							   + "<br>" + myDog.decisions[myDog.critic]);
-         updateDogOptions();
+		generatePositiveComment();
+        clearTimeout(timeOutID);
+        $('#pupper img').attr('src', "images/wag-pup.gif");
 	});
 	$('#bite').click(function() {
-		$('#comments p').html("<q>" + negativeResponses[parseInt((Math.random() * negativeResponses.length), 10)] + "</q>"
-							   + "<br>" + myDog.decisions[myDog.critic]);
-         updateDogOptions();
+		generateNegativeComment();
+        clearTimeout(timeOutID);
+        $('#pupper img').attr('src', "images/pup-bite.gif");
 	});
     $('#next-button').click(function() {
         //update text
         myDog.critic++;
-        $('#comments').hide();
-        actionOk = true;
-        $('#next-button').hide();
-        $('#instructions p').html("<br>" + criticNames[myDog.critic] + instructions);
-        //update animations
+        if (myDog.critic >= totalCritics) {
+            prepareResultPages();
+        } else {
+            $("#criticIcon").attr('src', criticIcons[myDog.critic]);
+            $('#comments').hide();
+            actionOk = true;
+            $('#next-button').hide();
+            $('#instructions p').html("<br>" + criticNames[myDog.critic] + instructions);
+            //update animations
+        }
     });
 }
 
@@ -133,13 +168,16 @@ function updateDogOptions() {
     $('#next-button').show();
     $('#comments').show();
 	if (actionOk) {
-        if (myDog.critic + 1 >= totalCritics) {
-            prepareResultPages();
+        if (myDog.critic + 1 == totalCritics) {
+            if (Math.random() <= myDog.adoptionChance) {
+                result = true; 
+            } else {
+                result = false;
+            }
         }
         $('#picture').text(myDog.critic);
     }
     actionOk = false;
-    
     
     //insert animations here
 }
